fc4bd0b24b01f056349dd19a6720ecd763613906
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-04-26 20:00:28 +0530

    Updating jenkins

--- seed-jobs/b_cart.groovy
+++ seed-jobs/b_cart.groovy
@@ -2,7 +2,7 @@ multibranchPipelineJob('CI-pipeline/cart') {
     branchSources {
         git {
             id('123456789') // IMPORTANT: use a constant and unique identifier
-            remote('https://github.com/Naren-Chakravarthy/jenkins.git')
+            remote('https://github.com/Naren-Chakravarthy/cart.git')
         }
     }
     orphanedItemStrategy {
